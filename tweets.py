d51172900bf3777f2a78598519c8a2942ed78b96
Sana Javed <sanajaved7@gmail.com>
2015-04-05 21:42:45 -0400

    Made formatting changes based on Flake8

--- tweets.py
+++ tweets.py
@@ -3,14 +3,18 @@ import csv
 from config import (consumer_key, consumer_secret,
                     access_token_key, access_token_secret)
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key,
-    access_token_secret)
+api = TwitterAPI(consumer_key, consumer_secret,
+                 access_token_key, access_token_secret)
+
 
 def twitter_search(twitter_query):
-    """ Pass in what you want to search for in Twitter and it creates a csv file with the tweets pulled from the API
+    """ Pass in what you want to search for in
+    Twitter and it creates a csv file with
+    the tweets pulled from the API
     """
-    request_tweets = api.request('search/tweets', {'q':twitter_query})
+    request_tweets = api.request('search/tweets', {'q': twitter_query})
     return request_tweets
+
 
 def prepare_tweets(tweet):
     """ Pass in tweet and returns list of elements of
@@ -29,8 +33,8 @@ def prepare_tweets(tweet):
 def make_csv(request_tweets):
     with open('twitterproj.csv', 'w') as csvfile:
         twitter_writer = csv.writer(csvfile)
-        twitter_writer.writerow(
-            ["Tweet Text",
+        twitter_writer.writerow([
+            "Tweet Text",
             "Number of Retweets",
             "User Name",
             "Number of Favorites",
@@ -38,7 +42,8 @@ def make_csv(request_tweets):
             "Date/Time of Tweet",
             "Hashtags in Tweet"])
         for tweet in request_tweets:
-             twitter_writer.writerow(prepare_tweets(tweet))
+            twitter_writer.writerow(prepare_tweets(tweet))
+
 
 def hashtags(list_of_hashtags):
     string = ""
