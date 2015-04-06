2c8c4fbad900639b2340ead02bdd560e3179f612
Sana Javed <sanajaved7@gmail.com>
2015-04-05 21:04:17 -0400

    Abstracted code to allow different queries

--- tweets.py
+++ tweets.py
@@ -1,38 +1,41 @@
 from TwitterAPI import TwitterAPI
 import csv
-from config import consumer_key, consumer_secret, access_token_key, access_token_secret
+from config import (consumer_key, consumer_secret,
+                    access_token_key, access_token_secret)
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key,
+    access_token_secret)
 
-
-request_tweets = api.request('search/tweets', {'q':'#blacklivesmatter'})
+def twitter_search(twitter_query):
+    """ Pass in what you want to search for in Twitter and it creates a csv file with the tweets pulled from the API
+    """
+    request_tweets = api.request('search/tweets', {'q':twitter_query})
+    with open('twitterproj.csv', 'w') as csvfile:
+        twitter_writer = csv.writer(csvfile)
+        twitter_writer.writerow(
+            ["Tweet Text",
+            "Number of Retweets",
+            "User Name",
+            "Number of Favorites",
+            "Location of User",
+            "Date/Time of Tweet",
+            "Hashtags in Tweet"])
+        for item in request_tweets:
+             twitter_writer.writerow([
+                item["text"],
+                item["retweet_count"],
+                item["user"]["name"],
+                item["favorite_count"],
+                item["user"]["location"],
+                item["created_at"],
+                hashtags(item["entities"]["hashtags"])
+            ])
 
 def hashtags(list_of_hashtags):
     string = ""
     for x in list_of_hashtags:
         string += "#" + x["text"] + " "
     return string
-
-with open('twitterproj.csv', 'w') as csvfile:
-    twitter_writer = csv.writer(csvfile)
-    twitter_writer.writerow(
-        ["Tweet Text",
-        "Number of Retweets",
-        "User Name",
-        "Number of Favorites",
-        "Location of User",
-        "Date/Time of Tweet",
-        "Hashtags in Tweet"])
-    for item in request_tweets:
-         twitter_writer.writerow([
-            item["text"],
-            item["retweet_count"],
-            item["user"]["name"],
-            item["favorite_count"],
-            item["user"]["location"],
-            item["created_at"],
-            hashtags(item["entities"]["hashtags"])
-        ])
 
 # "text"
 # "retweet_count"
@@ -41,3 +44,8 @@ with open('twitterproj.csv', 'w') as csvfile:
 # "location"
 # "created_at"
 # "hashtags"
+
+if __name__ == '__main__':
+    twitter_search("#blacklivesmatter")
+    # get twitter search , etc
+    # some_fucntion_that_processes_raw_input()
